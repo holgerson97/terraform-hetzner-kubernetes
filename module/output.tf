c5192a244d322bd39430188423bf86549abae659
Nils Carstensen <nilscarstensen@live.com>
2021-05-02 21:36:14 +0200

    Added: Output of subnets.

--- module/output.tf
+++ module/output.tf
@@ -45,3 +45,75 @@ output "network_labels" {
   value = one(hcloud_network.main[*].labels)
   
 }
+
+output "subnet_nodes_id" {
+
+  value = one(hcloud_network_subnet.nodes[*].id)
+  
+}
+
+output "subnet_nodes_network_id" {
+
+  value = one(hcloud_network_subnet.nodes[*].network_id)
+  
+}
+
+output "subnet_nodes_type" {
+
+  value = one(hcloud_network_subnet.nodes[*].type)
+  
+}
+
+output "subnet_nodes_ip_range" {
+
+  value = one(hcloud_network_subnet.nodes[*].ip_range)
+  
+}
+
+output "subnet_nodes_network_zone" {
+
+  value = one(hcloud_network_subnet.nodes[*].network_zone)
+  
+}
+
+output "subnet_nodes_vswitch_id" {
+
+  value = one(hcloud_network_subnet.nodes[*].vswitch_id)
+  
+}
+
+output "subnet_loadbalancer_id" {
+
+  value = one(hcloud_network_subnet.loadbalancer[*].id)
+  
+}
+
+output "subnet_loadbalancer_network_id" {
+
+  value = one(hcloud_network_subnet.loadbalancer[*].network_id)
+  
+}
+
+output "subnet_loadbalancer_type" {
+
+  value = one(hcloud_network_subnet.loadbalancer[*].type)
+  
+}
+
+output "subnet_loadbalancer_ip_range" {
+
+  value = one(hcloud_network_subnet.loadbalancer[*].ip_range)
+  
+}
+
+output "subnet_loadbalancer_network_zone" {
+
+  value = one(hcloud_network_subnet.loadbalancer[*].network_zone)
+  
+}
+
+output "subnet_loadbalancer_vswitch_id" {
+
+  value = one(hcloud_network_subnet.loadbalancer[*].vswitch_id)
+  
+}
