044d43263b1790446fea639e76508354510c3fdd
Nils Carstensen <nilscarstensen@live.com>
2021-05-04 22:56:18 +0200

    Add: Valid example configuration.

--- examples/main.tf
+++ examples/main.tf
@@ -24,15 +24,18 @@ module "kubernetes_cluster" {
 
   source = "../module"
 
+  kub_masters = 1
+  kub_slaves  = 3
+
   location           = "nbg1"
   server_type_master = "cpx11"
   server_type_slaves = "cx11"
   image              = "ubuntu-20.04"
 
-  root_ssh_key    = null
-  ansible_ssh_key = null
+  root_ssh_key    = "null"
+  ansible_ssh_key = "null"
 
-  kube_network_range = "10.10.0.0/24"
+  kub_network_range = "10.10.0.0/16"
   nodes_range        = "10.10.1.0/24"
   loadbalancer_range = "10.10.2.0/24"
 
